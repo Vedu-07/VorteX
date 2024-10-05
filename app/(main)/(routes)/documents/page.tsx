41f80ab9ee3c8419697ebae30b8673caba920240
Vedu-07 <vedantswami02@gmail.com>
2024-10-05 09:17:07 +0530

    Publish Functionality Added

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -29,7 +29,7 @@ const DocumentsPage = () => {
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image 
-      src="/empty.png"
+      src="/empty-light.png"
       height="300"
       width="300"
       alt="Empty"
--- hooks/use-origin.tsx
+++ hooks/use-origin.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+// This hook Is used to get the url of current window as it is gonna be used in Publish functionality
 export const useOrigin = () => {
   const [mounted, setMounted] = useState(false);
   const origin = typeof window !== "undefined" && window.location.origin ? window.location.origin : "";
